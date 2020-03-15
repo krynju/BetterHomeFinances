ba277f338ff584a83847e318d609452b7f0dafec
krynju <gulinski.krystian@gmail.com>
2020-03-15 21:38:04 +0100

    add login activity lib

--- app/src/main/java/com/example/betterhomefinances/MainActivity.kt
+++ app/src/main/java/com/example/betterhomefinances/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.AuthUI.IdpConfig.*
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+const val RC_SIGN_IN = 123;
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +20,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val auth = FirebaseAuth.getInstance()
+        val AuthUI = AuthUI.getInstance()
+
+
+        if (auth.currentUser != null) {
+            // already signed in
+        } else {
+            startActivityForResult(
+                AuthUI
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(
+                        listOf(
+                            GoogleBuilder().build(),
+                            EmailBuilder().build()
+                        )
+                    )
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
 
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
@@ -38,4 +65,33 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+    if (requestCode == RC_SIGN_IN) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        // Successfully signed in
+        if (resultCode == RESULT_OK) {
+            startActivity(SignedInActivity.createIntent(this, response));
+            finish();
+        } else {
+            // Sign in failed
+            if (response == null) {
+                // User pressed back button
+                showSnackbar(R.string.sign_in_cancelled);
+                return;
+            }
+
+            if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                showSnackbar(R.string.no_internet_connection);
+                return;
+            }
+
+            showSnackbar(R.string.unknown_error);
+            Log.e(TAG, "Sign-in error: ", response.getError());
+        }
+    }
+}*/
 }
