3895eddfaa5ee4ed7fa5a4ed702fc96b085591e2
krynju <gulinski.krystian@gmail.com>
2020-03-22 10:55:16 +0100

    sign up worked (added keys)

--- app/src/main/java/com/example/betterhomefinances/MainActivity.kt
+++ app/src/main/java/com/example/betterhomefinances/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.betterhomefinances
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 const val RC_SIGN_IN = 123;
 
@@ -66,7 +68,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+
+                val intent = Intent(this, LoggedInActivity::class.java)
+                startActivity(intent);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+//                    val snack = Snackbar.make(, "snackbar",Snackbar.LENGTH_LONG)
+//                    // User pressed back button
+//                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getError()!!.getErrorCode() == ErrorCodes.NO_NETWORK) {
+//                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+//                showSnackbar(R.string.unknown_error);
+//                Log.e(TAG, "Sign-in error: ", response.getError());
+            }
+        }
+    }
+
+
+    /*
+    *     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
     // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
     if (requestCode == RC_SIGN_IN) {
